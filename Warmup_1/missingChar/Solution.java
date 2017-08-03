23dffc60b26c64bc9806aaac109433f0f39f0a85
Sevak Mardirosian <smardirosians@gmail.com>
2017-08-03 12:07:28 +0200

    missingchar

--- Warmup_1/missingChar/Solution.java
+++ Warmup_1/missingChar/Solution.java
@@ -1,10 +1,24 @@
 package missingChar;
 
+/**
+ * 
+ * @author sevmardi 
+ * Objective:Given a non-empty string and an int n, return a
+ *         new string where the char at index n has been removed. The value of n
+ *         will be a valid index of a char in the original string (i.e. n will
+ *         be in the range 0..str.length()-1 inclusive).
+ */
 public class Solution {
 
 	public static void main(String[] args) {
-		
+		System.out.println(missingChar("kiten", 1));
 	}
-	
-	
+
+	public static String missingChar(String str, int n) {
+		String font = str.substring(0, n);
+		String back = str.substring(n + 1, str.length());
+
+		return (font + back);
+	}
+
 }
